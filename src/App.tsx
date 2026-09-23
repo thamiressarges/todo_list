03e30e4c057f7588b6949912fc3c2ccbf70ce87b
Thamires Sarges <thamires.sarges.58@gmail.com>
2026-09-23 10:42:08 -0400

    feat: adiciona header e ajusta layout da lista de tarefas

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,17 @@ import './App.css'
 function App() {
   return (
     <div className='container-app'>
-      <h1>Tarefas</h1>
-      <input type="text" placeholder='Adicionar nova tarefa' />
 
-      <ul className='content-tasks'>
-        <li className='task-item'>Tarefa 1</li>
-        <li className='task-item'>Tarefa 2</li>
-        <li className='task-item'>Tarefa 3</li>
-      </ul>
+      <div className='container-app__header'>
+         <h1>Tarefas</h1>
+        <input type="text" placeholder='Adicionar nova tarefa' />
+      </div>
+
+        <ul className='content-tasks'>
+          <li className='task-item'>Tarefa 1</li>
+          <li className='task-item'>Tarefa 2</li>
+          <li className='task-item'>Tarefa 3</li>
+        </ul>
     
     </div>
   )
